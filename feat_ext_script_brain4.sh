c8356ca44706d29cb8ccd85207cfd976df95f724
KSL <49044902+KarlSangwon@users.noreply.github.com>
2021-05-27 10:37:21 -0400

    Update feat_ext_script_brain4.sh

--- feat_ext_script_brain4.sh
+++ feat_ext_script_brain4.sh
@@ -3,15 +3,19 @@
 
 # Bash Script to Apply Feature Extraction Script to .npy probability map Files
 
-# Path to ZhenZhen's Feature Extraction
+# [CHANGE THIS] Path to ZhenZhen's Feature Extraction
 feature_ext=/cis/home/slee508/ADproject-master/FeatureExtraction/feature_extraction.py
+
+
+
+# only input & output paths have to change for different folders
 
 
 ################# REPEAT (COPY & PASTE & EDIT) THIS CODE BLOCK FOR EACH FOLDER #################
 
-# Path to Probability Map from applying UNet
+# [CHANGE THIS] Path to Probability Map from applying UNet
 inpath="/cis/home/slee508/my_documents/Brain4/prob_map_data/block1/"
-# Path to output assets
+# [CHANGE THIS] Path to output assets
 outpath="/cis/home/slee508/my_documents/Brain4/asset/block1/"
 
 echo "------------------------------------------------"
@@ -41,4 +45,42 @@ echo -e "Output 2 will be saved as : $output_name2.csv"
 done
 
 ################# REPEAT (COPY & PASTE & EDIT) THIS CODE BLOCK FOR EACH FOLDER #################
+
+
+
+
+
+# example of repeat
+
+################# REPEAT (COPY & PASTE & EDIT) THIS CODE BLOCK FOR EACH FOLDER #################
+
+inpath="/cis/home/slee508/my_documents/Brain4/prob_map_data/block2/"
+outpath="/cis/home/slee508/my_documents/Brain4/asset/block2/"
+
+echo "------------------------------------------------"
+echo "New Input Path: " "$inpath"
+echo -e "New Output Path: " "$outpath\n"
+
+for file in "$inpath"*.npy
+
+do
+
+files="${file##*/}"
+file_name="${files%.*}"
+output_name1="Segmented_$file_name"
+output_name2="description_$file_name"
+
+echo -e "Input File is: $files\n"
+echo "Performing Feature Extraction"
+
+nohup python "$feature_ext" "$file" "$outpath$output_name1.npy" "$outpath$output_name2.csv" >/dev/null 2>&1 &
+
+echo "Nohup script submitted"
+echo -e "Output 1 will be saved as : $output_name1.npy"
+echo -e "Output 2 will be saved as : $output_name2.csv"
+
+done
+
+################# REPEAT (COPY & PASTE & EDIT) THIS CODE BLOCK FOR EACH FOLDER #################
+
 
