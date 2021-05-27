31105d0c1ad9e1de26a3dd05a00aa42b4f5b9cfa
KSL <49044902+KarlSangwon@users.noreply.github.com>
2021-05-27 10:35:14 -0400

    Update unet_script_brain4.sh

--- unet_script_brain4.sh
+++ unet_script_brain4.sh
@@ -3,18 +3,53 @@
 
 # Bash Script to Apply UNet Script to Tif Files
 
-# Path to ZhenZhen's UNet apply script
+# [CHANGE THIS] Path to ZhenZhen's UNet apply script
 apply=/cis/home/slee508/ADproject-master/unet/apply.py
-# Path to Trained UNet Model
+# [CHANGE THIS] Path to Trained UNet Model
 unet=/cis/home/slee508/ADproject-master/unet/asset/unet_tau.pth
 
 
+
+# for different folders, only input & output paths have to change.
+
 ################# REPEAT (COPY & PASTE & EDIT) THIS CODE BLOCK FOR EACH FOLDER #################
 
-# Input Path (.tif files)
+# [CHANGE THIS] Input Path (.tif files) 
 inpath="/cis/home/kstouff4/Documents/datasets/exvivohuman_11T/more_blocks/Brain4/histology/down_000/AD_Hip1/Tau/"
-# Output Path (.npy files)
+# [CHANGE THIS] Output Path (.npy files)
 outpath="/cis/home/slee508/my_documents/Brain4/prob_map_data/block1/"
+
+echo "------------------------------------------------"
+echo "New Input Path: " "$inpath"
+echo -e "New Output Path: " "$outpath\n"
+
+for file in "$inpath"*crop.tif
+
+do
+
+files="${file##*/}"
+file_name="${files%.*}"
+output_name="pmap_$file_name"
+
+echo -e "Input File is: $files\n"
+echo "Applying UNET"
+python "$apply" "$file" "$unet" "$outpath$output_name.npy"
+
+echo -e "Output Probability Map Saved as: $output_name.npy\n"
+
+done
+
+################# REPEAT (COPY & PASTE & EDIT) THIS CODE BLOCK FOR EACH FOLDER #################
+
+
+
+
+# example of repeat (for block 2)
+
+################# REPEAT (COPY & PASTE & EDIT) THIS CODE BLOCK FOR EACH FOLDER #################
+
+inpath="/cis/home/kstouff4/Documents/datasets/exvivohuman_11T/more_blocks/Brain4/histology/down_000/AD_Hip2/Tau/"
+outpath="/cis/home/slee508/my_documents/Brain4/prob_map_data/block2/"
 
 echo "------------------------------------------------"
 echo "New Input Path: " "$inpath"
