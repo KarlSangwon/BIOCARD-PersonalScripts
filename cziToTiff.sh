f200f35cb36094b3499a7e72139aa65c2fe7d2f7
KSL <49044902+KarlSangwon@users.noreply.github.com>
2021-05-27 10:32:59 -0400

    Update cziToTiff.sh

--- cziToTiff.sh
+++ cziToTiff.sh
@@ -39,3 +39,34 @@ done
 
 ################# REPEAT (COPY & PASTE & EDIT) THIS CODE BLOCK FOR EACH FOLDER #################
 
+
+
+# example of repeat: now for block4 (AD_Hip4 folder)
+
+################# REPEAT (COPY & PASTE & EDIT) THIS CODE BLOCK FOR EACH FOLDER #################
+
+
+inpath="/cis/home/zhou/Share/HRMRI/Histology/Brain4/Block 4/6E10/"
+outpath="/cis/home/kstouff4/Documents/datasets/exvivohuman_11T/more_blocks/Brain4/histology/down_000/AD_Hip4/Amyloid/"
+
+sed '23d' "$f" > "$tmp" && mv "$tmp" "$f"
+
+echo "Changing Path"
+echo "New Input Path: " "$inpath"
+echo "New Output Path: " "$outpath"
+
+sed "23i outpath = '$outpath'" "$f" > "$tmp" && mv "$tmp" "$f"
+
+for file in "$inpath"*.czi
+do
+
+echo "Converting: "$file
+
+/cis/home/slee508/Fiji.app/ImageJ-linux64 --ij2 --headless --console --run cziToTif_v01.py "FILENAME='$file'"
+
+done
+
+################# REPEAT (COPY & PASTE & EDIT) THIS CODE BLOCK FOR EACH FOLDER #################
+
+
+
