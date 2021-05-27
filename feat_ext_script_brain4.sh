f924beb83ac5aa3c56616477e0b2f0149fa7d5d8
KSL <49044902+KarlSangwon@users.noreply.github.com>
2021-05-27 10:10:51 -0400

    Update feat_ext_script_brain4.sh

--- feat_ext_script_brain4.sh
+++ feat_ext_script_brain4.sh
@@ -1,9 +1,17 @@
-# Feature Extraction
+# AUTHOR: (Karl) Sangwon Lee
+# UPDATED: May 27, 2021
 
+# Bash Script to Apply Feature Extraction Script to .npy probability map Files
+
+# Path to ZhenZhen's Feature Extraction
 feature_ext=/cis/home/slee508/ADproject-master/FeatureExtraction/feature_extraction.py
 
 
+################# REPEAT (COPY & PASTE & EDIT) THIS CODE BLOCK FOR EACH FOLDER #################
+
+# Path to Probability Map from applying UNet
 inpath="/cis/home/slee508/my_documents/Brain4/prob_map_data/block1/"
+# Path to output assets
 outpath="/cis/home/slee508/my_documents/Brain4/asset/block1/"
 
 echo "------------------------------------------------"
@@ -22,6 +30,8 @@ output_name2="description_$file_name"
 echo -e "Input File is: $files\n"
 echo "Performing Feature Extraction"
 
+
+# Submit Feature Extraction Script to the machine. 
 nohup python "$feature_ext" "$file" "$outpath$output_name1.npy" "$outpath$output_name2.csv" >/dev/null 2>&1 &
 
 echo "Nohup script submitted"
@@ -29,3 +39,6 @@ echo -e "Output 1 will be saved as : $output_name1.npy"
 echo -e "Output 2 will be saved as : $output_name2.csv"
 
 done
+
+################# REPEAT (COPY & PASTE & EDIT) THIS CODE BLOCK FOR EACH FOLDER #################
+
